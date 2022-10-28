9bb6f66797c951c96ab0b368eede6689a071cd6a
NickMan016 <npcastillo73@gmail.com>
2022-10-28 11:28:09 -0500

    Componente del Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+
+export const Footer = () => {
+    return(
+        <div className="bg-[#1a1a1a] mt-10">
+            <div className="max-w-[1100px] w-[90%] m-auto py-5">
+                <span className="">
+                    Copyright © 2022 ForoMovies. All rights reserved.
+                </span>
+                <span className="float-right">
+                    By <strong>NickMan016</strong>
+                </span>
+            </div>
+        </div>
+    )
+}
