4133bfed3fa0149ab06cd3ce7599b3438e1d976f
NickMan016 <npcastillo73@gmail.com>
2022-11-03 19:41:30 -0600

    Borrado de fondo en Carousel

--- src/components/Carousel/components/ItemsCarousel/index.tsx
+++ src/components/Carousel/components/ItemsCarousel/index.tsx
@@ -7,10 +7,10 @@ interface ItemCarouselProps {
 
 export const ItemCarousel = ({ id, srcImage }: ItemCarouselProps) => {
     return(
-        <div className="bg-[#ff8000]">
+        <>
             <Link to={`/movie/${id}`} className="block w-full m-auto">
                 <img src={`https://image.tmdb.org/t/p/w500/${srcImage}`} className="block w-full" />
             </Link>
-        </div>
+        </>
     )
 }
